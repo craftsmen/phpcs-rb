44c8fc85ce0facef6835391d41447f69760918c7
kevcha <kevin.chavanne@gmail.com>
2014-11-05 17:14:13 +0100

    Add tests * Add Rakefile * Add phpcs_test.rb * Add test/fixtures/foo.php file for testing purpose

---
+++ test/phpcs_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'phpcs'
+
+class PhpcsTest < Minitest::Test
+  def test_foo
+    file = File.open('test/fixtures/foo.php')
+    results = Phpcs.lint(file.read)
+
+    assert_equal results[0].line, 3
+    assert_equal results[0].comment, 'Each class must be in a namespace of at least one level (a top-level vendor name)'
+    assert_equal results[1].line, 3
+    assert_equal results[1].comment, 'Opening brace of a class must be on the line after the definition'
+    assert_equal results[2].line, 7
+    assert_equal results[2].comment, 'A closing tag is not permitted at the end of a PHP file'
+  end
+end
